94e0a05d8f0bc9087131d41f53f6a64331ed6af2
chrislucas <christoffer.luccas@gmail.com>
2018-07-30 01:28:54 -0300

    primeira solucao

--- src/i_introduction/_1_Java_To_Kotlin_Converter/n01JavaToKotlinConverter.kt
+++ src/i_introduction/_1_Java_To_Kotlin_Converter/n01JavaToKotlinConverter.kt
@@ -13,6 +13,12 @@ fun todoTask1(collection: Collection<Int>): Nothing = TODO(
     references = { JavaCode1().task1(collection) })
 
 
+
 fun task1(collection: Collection<Int>): String {
-    todoTask1(collection)
+    return "teste"
+}
+
+
+fun main(args: Array<String>) {
+
 }
